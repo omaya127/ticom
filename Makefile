899890268905c2c763835852ce1e9d7ba5b5a893
omaya127 <omaya127@163.com>
2020-05-27 08:40:51 -0700

    init

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -o ticom ticom.c
--- ticom.c
+++ ticom.c
@@ -1,23 +1,71 @@
+#include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <termios.h>
 
 int tty_fd = -1;
-int stdin_fd = 0;
-int stdout_fd = 1;
 
 int main(int argc, char *argv[])
 {
-    tty_fd = open(arvg[1], O_RDWR | O_NONBLOCK | O_NOCTTY);
+    struct termios term;
+
+    tty_fd = open(argv[1], O_RDWR | O_NONBLOCK | O_NOCTTY);
     if (tty_fd < 0) {
         printf("open tty failed!\n"); 
         return -1;
     }
 
-    struct timeval *ptv;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &term);
+
+    tcgetattr(tty_fd, &term);
+    // baud
+    cfsetispeed(&term, 115200);
+    cfsetospeed(&term, 115200);
+    // data bit
+    term.c_cflag &= ~CSIZE;
+    term.c_cflag |= CS8;
+    // check bit
+    term.c_cflag &= ~PARENB;
+    // stop bit
+    term.c_cflag &= ~CSTOPB;
+    tcflush (tty_fd, TCIFLUSH);
+    tcsetattr(tty_fd, TCSANOW, &term);
+
+    //struct timeval *ptv;
+    int ret, nbytes;
+    fd_set rset, wset;
+
     for (;;) {
         FD_ZERO(&rset);
         FD_ZERO(&wset);
-        FD_SET()
+        FD_SET(tty_fd, &rset);
+        FD_SET(STDIN_FILENO, &rset);
+
+        ret = select(tty_fd + 1, &rset, NULL, NULL, NULL);
+
+        if (ret == -1) {
+            printf("select error\n");
+            return -1;
+        }
+
+        /* read from term */
+        if (FD_ISSET(STDIN_FILENO, &rset)) {
+            char buf[32];
+            nbytes = read(STDIN_FILENO, buf, 32);
+            if (nbytes > 0) {
+                write(tty_fd, buf, nbytes);
+            }
+        }
+        if (FD_ISSET(tty_fd, &rset)) {
+            char buf[128];
+            nbytes = read(tty_fd, buf, 128);
+            if (nbytes > 0) {
+                write(STDOUT_FILENO, buf, nbytes);
+            }
+        }
     }
 }
